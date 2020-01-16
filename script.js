1d17e6f351a0ab264ab6b47d7a493effe4de3f1c
C. Stanford Brown <seiyu32@gmail.com>
2020-01-17 04:27:50 +0900

    Add initial Message files

---
+++ script.js
@@ -0,0 +1,20 @@
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+
+
+sendBtn.addEventListener('click', sendMsg);
+
+
+function sendMsg (){
+    let content = messageIn.value;
+    if(content === ''){alert('Enter Something Stupid!')
+    }
+    else{
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+
+
+}
